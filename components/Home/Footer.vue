a96277437fce45ed15d4791c2550be86f5dba1d5
Sugar_F0x <sugarfox.dev@gmail.com>
2021-08-13 10:51:16 +0300

    :art: Adjust Footer cards positioning (add cols)

--- components/Home/Footer.vue
+++ components/Home/Footer.vue
@@ -1,33 +1,41 @@
 <template>
   <footer>
-    <Card color='primary' icon='mdi-phone' class='phone-card'>
-      <template #title>Позвоните нам</template>
-      <template #text>
-        <ul class='list'>
-          <li><a href='tel:88002553535'>+7 (915) 150-34-45</a></li>
-          <li><a href='tel:89852252525'>+7 (985) 225-25-25</a></li>
-        </ul>
-      </template>
-    </Card>
-    <Card color='secondary' icon='mdi-email'>
-      <template #title>Напишите нам</template>
-      <template #text>
-        <ul class='list'>
-          <li><a href='mailto:example@example.ru'>example@example.ru</a></li>
-          <li><a href='mailto:sugarfox@sugarfox.ru'>sugarfox@sugarfox.ru</a></li>
-        </ul>
-      </template>
-    </Card>
-    <Card color='primary' icon='mdi-book'>
-      <template #title>Приезжайте к нам</template>
-      <template #text>
-        <p>
-          Московская область, Пушкинский район
-          <br>
-          р-н Лесные поляны, м-н Полянка, д28а
-        </p>
-      </template>
-    </Card>
+    <v-row no-gutters>
+      <v-col cols='12' md='4'>
+        <Card color='primary' icon='mdi-phone' class='phone-card'>
+          <template #title>Позвоните нам</template>
+          <template #text>
+            <ul class='list'>
+              <li><a href='tel:88002553535'>+7 (915) 150-34-45</a></li>
+              <li><a href='tel:89852252525'>+7 (985) 225-25-25</a></li>
+            </ul>
+          </template>
+        </Card>
+      </v-col>
+      <v-col cols='12' md='4'>
+        <Card color='secondary' icon='mdi-email'>
+          <template #title>Напишите нам</template>
+          <template #text>
+            <ul class='list'>
+              <li><a href='mailto:example@example.ru'>example@example.ru</a></li>
+              <li><a href='mailto:sugarfox@sugarfox.ru'>sugarfox@sugarfox.ru</a></li>
+            </ul>
+          </template>
+        </Card>
+      </v-col>
+      <v-col cols='12' md='4'>
+        <Card color='primary' icon='mdi-book'>
+          <template #title>Приезжайте к нам</template>
+          <template #text>
+            <p>
+              Московская область, Пушкинский район
+              <br>
+              р-н Лесные поляны, м-н Полянка, д28а
+            </p>
+          </template>
+        </Card>
+      </v-col>
+    </v-row>
   </footer>
 </template>
 
