0ed2e56a0d5bcd03a82d9e77822e6045566ae9f1
Sugar_F0x <sugarfox.dev@gmail.com>
2021-08-13 10:49:43 +0300

    :art: Adjust Work section position (fill width)

--- components/Home/Work.vue
+++ components/Home/Work.vue
@@ -1,36 +1,34 @@
 <template>
   <section class='work'>
-    <v-container class='pa-0'>
-      <v-row no-gutters>
-        <v-col cols='12' md='4'>
-          <Card color='primary' icon='mdi-crane'>
-            <template #title> Титул и всякое </template>
-            <template #text>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Adipisci autem, consequatur eos maxime nobis repellendus.
-              Asperiores beatae cupiditate eligendi sapiente!
-            </template>
-          </Card>
-        </v-col>
-        <v-col cols='12' md='4'>
-          <Card color='secondary' icon='mdi-account-hard-hat'>
-            <template #title>Интересное название</template>
-            <template #text>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, voluptatibus.
-            </template>
-          </Card>
-        </v-col>
-        <v-col cols='12' md='4'>
-          <Card color='primary' icon='mdi-pencil-ruler'>
-            <template #title>Что-то важное</template>
-            <template #text>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
-              aliquam consectetur dignissimos, magni quibusdam sapiente.
-            </template>
-          </Card>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-row no-gutters>
+      <v-col cols='12' md='4'>
+        <Card color='primary' icon='mdi-crane'>
+          <template #title> Титул и всякое </template>
+          <template #text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Adipisci autem, consequatur eos maxime nobis repellendus.
+            Asperiores beatae cupiditate eligendi sapiente!
+          </template>
+        </Card>
+      </v-col>
+      <v-col cols='12' md='4'>
+        <Card color='secondary' icon='mdi-account-hard-hat'>
+          <template #title>Интересное название</template>
+          <template #text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae, voluptatibus.
+          </template>
+        </Card>
+      </v-col>
+      <v-col cols='12' md='4'>
+        <Card color='primary' icon='mdi-pencil-ruler'>
+          <template #title>Что-то важное</template>
+          <template #text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+            aliquam consectetur dignissimos, magni quibusdam sapiente.
+          </template>
+        </Card>
+      </v-col>
+    </v-row>
   </section>
 </template>
 
